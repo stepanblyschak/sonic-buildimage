2b8b295c067820f8e4f085f7ddcbd004047743ac
Stepan Blyschak <stepanb@nvidia.com>
2020-09-22 10:38:34 +0000

    [docker-syncd-bfn] add manifest for docker-syncd-bfn
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- platform/barefoot/docker-syncd-bfn.mk
+++ platform/barefoot/docker-syncd-bfn.mk
@@ -3,6 +3,7 @@
 DOCKER_SYNCD_PLATFORM_CODE = bfn
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
+$(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
 
 $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
