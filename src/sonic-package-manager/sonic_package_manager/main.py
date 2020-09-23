c131c164b05efbcc9fdc2794b248ac40805dcaaa
Stepan Blyschak <stepanb@nvidia.com>
2020-09-23 11:11:05 +0000

    [sonic-package-manager] add/remove packages CLI
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- src/sonic-package-manager/sonic_package_manager/main.py
+++ src/sonic-package-manager/sonic_package_manager/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import click
 import tabulate
 import natsort
@@ -22,6 +23,8 @@ def cli():
 
 @cli.command()
 def list():
+    ''' List available packages. '''
+
     db = database.PackageDatabase()
     table_header = ["Name", "Repository", "Description", "Version", "Status"]
     table_body = []
@@ -40,6 +43,8 @@ def list():
 @cli.command()
 @click.argument("name")
 def describe(name):
+    ''' Print the manifest content. '''
+
     try:
         db = database.PackageDatabase()
         pkg = db.get_package(name)
@@ -48,34 +53,73 @@ def describe(name):
         click.echo(yaml.safe_dump(pkg.get_manifest()))
     except PackageManagerError as err:
         click.secho("Failed to describe package {}: {}".format(name, err), fg="red")
+        sys.exit(1)
+
+
+@cli.command()
+@click.argument("name")
+@click.argument("repository")
+@click.argument("default-version")
+def add(name, repository, default_version):
+    ''' Add a new package to database. '''
+
+    if os.geteuid() != 0:
+        click.secho("Root privileges required for this operation", fg="red")
+        sys.exit(1)
+    try:
+        db = database.PackageDatabase()
+        db.add_package(name, repository, description=None, default_version=default_version)
+    except PackageManagerError as err:
+        click.secho("Failed to add package {}: {}".format(name, err), fg="red")
+        sys.exit(1)
+
+
+@cli.command()
+@click.argument("name")
+def remove(name):
+    ''' Remove a package from database. '''
+
+    if os.geteuid() != 0:
+        click.secho("Root privileges required for this operation", fg="red")
+        sys.exit(1)
+    try:
+        db = database.PackageDatabase()
+        db.remove_package(name)
+    except PackageManagerError as err:
+        click.secho("Failed to remove package {}: {}".format(name, err), fg="red")
+        sys.exit(1)
 
 
 @cli.command()
 @click.argument("name")
 def install(name):
+    ''' Install a package. '''
+
     if os.geteuid() != 0:
         click.secho("Root privileges required for this operation", fg="red")
-        raise click.Abort()
+        sys.exit(1)
     try:
         installation = PackageInstallation(name)
         installation.install_package()
     except PackageManagerError as err:
         click.secho("Failed to install package {}: {}".format(name, err), fg="red")
-        return
+        sys.exit(1)
 
 
 @cli.command()
 @click.argument("name")
 def uninstall(name):
+    ''' Uninstall a package. '''
+
     if os.geteuid() != 0:
         click.secho("Root privileges required for this operation", fg="red")
-        raise click.Abort()
+        sys.exit(1)
     try:
         installation = PackageInstallation(name)
         installation.uninstall_package()
     except PackageManagerError as err:
         click.secho("Failed to uninstall package {}: {}".format(name, err), fg="red")
-        return
+        sys.exit(1)
     click.secho("Package {} succesfully uninstalled!".format(name), fg="green")
 
 
