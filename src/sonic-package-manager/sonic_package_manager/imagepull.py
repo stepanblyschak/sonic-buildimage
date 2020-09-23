8916cdc88323a80b41499540e998a5d785472e19
Stepan Blyschak <stepanb@nvidia.com>
2020-09-23 10:42:42 +0000

    [sonic-package-manager] change from low level api to docker sdk api
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- src/sonic-package-manager/sonic_package_manager/imagepull.py
+++ src/sonic-package-manager/sonic_package_manager/imagepull.py
@@ -15,41 +15,25 @@ class ImagePull(Operation):
         ''' Initialize ImagePull instance.
 
         Args:
-            self (ImagePull): ImagePull instance.
             package (Package): SONiC package object.
             version (str): SONiC package version to install.
         Returns:
             None.
         '''
 
-        self._client = docker.APIClient()
-        self._sdk_client = docker.from_env()
+        self._client = docker.from_env()
         self._package = package
         self._version = version
 
     def execute(self):
         ''' Execute the operation, pull the docker image associated
-            with this package from Docker registry.
+            with this package from Docker registry.'''
 
-        Args:
-            self (ImagePull): ImagePull instance.
-        Returns:
-            None.
-        '''
-
+        images = self._client.images
         try:
-            with click.progressbar(length=100) as bar:
-                stream = self._client.pull(self._package.get_repository(),
-                        tag=self._version, stream=True, decode=True)
-                get_logger().info('Downloading image from {}'.format(self._package.get_repository()))
-                for line in stream:
-                    if 'progressDetail' in line and 'current' in line['progressDetail']:
-                        bar.update(int(line['progressDetail']['current']) * 100 / int(line['progressDetail']['total']))
-                bar.update(100)
-
-            # Tag as latest
-            self._client.tag('{}'.format(self._get_image_name(self._version)),
-                    self._package.get_repository(), tag='latest')
+            get_logger().info('Downloading image {}'.format(self._package.get_repository()))
+            image = images.pull(self._package.get_repository(), self._version)
+            image.tag(self._package.get_repository(), 'latest')
         except docker.errors.APIError as err:
             self.restore()
             raise err
@@ -58,20 +42,39 @@ class ImagePull(Operation):
         ''' Restore the image pull operation. '''
 
         self._remove_runnnig_instances()
-
-        for tag in ('latest', self._version):
-            get_logger().info('Removing {}'.format(self._get_image_name(tag)))
-            self._client.remove_image('{}'.format(self._get_image_name(tag)), True)
+        self._remove_installed_images()
 
     def _get_image_name(self, tag):
-        ''' Returns the image name. '''
+        ''' Returns the image reference as <repository>:<tag>.
+
+        Args:
+            tag (str): An image tag.
+
+        Returns:
+            None.
+        '''
 
         return '{}:{}'.format(self._package.get_repository(), tag)
 
     def _remove_runnnig_instances(self):
-        ''' Remove all running containers created from the package image. '''
+        ''' Remove all running containers created
+            from the package image. '''
 
-        for container in self._sdk_client.containers.list(all=True):
-            if container.attrs['Config']['Image'] == self._get_image_name('latest'):
+        containers = self._client.containers
+        for container in containers.list(all=True):
+            container_image = container.attrs['Config']['Image']
+            if container_image == self._get_image_name('latest'):
                 container.remove(force=True)
+
+    def _remove_installed_images(self):
+        ''' Removes all installed repository tags of a package. '''
+
+        images = self._client.images
+        for image in images.list(all=True):
+            repotags = image.attrs['RepoTags']
+            for tag in repotags:
+                if (tag == self._get_image_name(self._version) or
+                    tag == self._get_image_name('latest')):
+                    get_logger().info('Removing {}'.format(tag))
+                    images.remove(image=tag, force=True)
 
