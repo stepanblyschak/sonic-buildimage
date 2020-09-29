7e0bc7acafaf90d8937899662b062a1f23d13fbf
Stepan Blyschak <stepanb@nvidia.com>
2020-09-29 19:33:49 +0000

    [sonic-package-manager] fix feature test
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- src/sonic-package-manager/tests/test_feature_registration.py
+++ src/sonic-package-manager/tests/test_feature_registration.py
@@ -10,7 +10,7 @@ from sonic_package_manager.package import Package
 from sonic_package_manager.errors import *
 
 
-def test_feature_installation():
+def test_feature_installation_no_feature_config():
     conn = mock.Mock(swsssdk.ConfigDBConnector)
     repo = mock.Mock(Repository)
     package = mock.Mock(Package)
@@ -20,6 +20,7 @@ def test_feature_installation():
     repo.get_package = mock.MagicMock(return_value=package)
 
     version = '1.0.0'
+    conn.get_entry = mock.Mock(return_value={})
     feature.register_feature(conn, repo, version)
     conn.mod_entry.assert_called_once_with('FEATURE', 'test',
         {'state': 'disabled',
@@ -27,6 +28,21 @@ def test_feature_installation():
          'auto_restart': 'enabled',
          'high_mem_alert': 'disabled'}
     )
+
+
+def test_feature_installation_existing_feature_config():
+    conn = mock.Mock(swsssdk.ConfigDBConnector)
+    repo = mock.Mock(Repository)
+    package = mock.Mock(Package)
+    feature.run_command = mock.Mock()
+
+    package.get_feature_name = mock.MagicMock(return_value='test')
+    repo.get_package = mock.MagicMock(return_value=package)
+
+    version = '1.0.0'
+    conn.get_entry = mock.Mock(return_value={'FEATURE': {'test': {}}})
+    feature.register_feature(conn, repo, version)
+    conn.mod_entry.assert_not_called()
 
 
 def test_feature_uninstallation():
