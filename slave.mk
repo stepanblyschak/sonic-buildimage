8932687d1767fd63a0f31c242ce293d81e25729d
Stepan Blyshchak <stepanb@nvidia.com>
2020-10-28 18:06:12 +0200

    [dockers] manifest for all SONiC dockers
    Signed-off-by: Stepan Blyshchak <stepanb@nvidia.com>

--- slave.mk
+++ slave.mk
@@ -714,6 +714,7 @@ $(addprefix $(TARGET_PATH)/, $(DOCKER_IMAGES)) : $(TARGET_PATH)/%.gz : .platform
 			--build-arg uid=$(UID) \
 			--build-arg guid=$(GUID) \
 			--build-arg docker_container_name=$($*.gz_CONTAINER_NAME) \
+			--build-arg docker_image_version=$($*.gz_VERSION) \
 			--build-arg frr_user_uid=$(FRR_USER_UID) \
 			--build-arg frr_user_gid=$(FRR_USER_GID) \
 			--label Tag=$(SONIC_IMAGE_VERSION) \
