b327100d76fbe634289da9ff6fa6b61b20e8f865
Stepan Blyschak <stepanb@nvidia.com>
2025-10-26 15:16:49 +0200

    Merge stepanblyschak:144
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- src/sonic-ctrmgrd-rs/crates/docker-wait-any-rs/src/main.rs
+++ src/sonic-ctrmgrd-rs/crates/docker-wait-any-rs/src/main.rs
@@ -33,7 +33,10 @@ struct Cli {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let identity = CString::new("docker-wait-any-rs").unwrap();
+    const DEFAULT_DATABASE_GLOBAL_CONFIG_PATH: &'static str = "/var/run/redis/sonic-db/database_global.json";
+    swss_common::sonic_db_config_initialize_global(DEFAULT_DATABASE_GLOBAL_CONFIG_PATH, true)?;
+
+    let identity = CString::new("docker-wait-any-rs").expect("CString::new failed");
     let syslog = syslog_tracing::Syslog::new(
         identity,
         syslog_tracing::Options::LOG_PID,
